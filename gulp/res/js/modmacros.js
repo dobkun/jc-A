a3828eb38885ebc5e900d232fc447e58ab2c3ce6
blackchandev <183952445+anthro-main@users.noreply.github.com>
2025-03-31 17:59:36 -0700

    add a ban template macro

--- gulp/res/js/modmacros.js
+++ gulp/res/js/modmacros.js
@@ -2,10 +2,10 @@ class modMacroHandler {
 	constructor(dropdown) {
 		this.dropdown = dropdown;
 		this.dropdown.selectedIndex = 0; // reset on page refresh, but let it stick
-		
+
 		this.inputMap = new Map();
 		const inputs = document.querySelectorAll('.actions input');
-		
+
 		inputs.forEach(input => {
 			this.inputMap.set(input.name, input);
 			input.addEventListener('change', () => {
@@ -16,16 +16,16 @@ class modMacroHandler {
 				switch (input.type) {
 					case 'text':
 						if (input.value !== '') {
-							parent.classList.add('modified');	
+							parent.classList.add('modified');
 						} else {
-							parent.classList.remove('modified');	
+							parent.classList.remove('modified');
 						}
 						break;
 					case 'checkbox':
 						if (input.checked) {
-							parent.classList.add('modified');	
+							parent.classList.add('modified');
 						} else {
-							parent.classList.remove('modified');	
+							parent.classList.remove('modified');
 						}
 						break;
 				}
@@ -35,7 +35,7 @@ class modMacroHandler {
 		this.reset();
 		dropdown.addEventListener('change', e => this.change(e));
 	}
-	
+
 	reset() {
 		for (const input of this.inputMap.values()) {
 			switch (input.type) {
@@ -47,9 +47,9 @@ class modMacroHandler {
 					input.value = '';
 					input.dispatchEvent(new Event('change', { bubbles: true }));
 			}
-		}	
+		}
 	}
-	
+
 	set(name, value) {
 		if (!name) {
 			return;
@@ -68,11 +68,11 @@ class modMacroHandler {
 		}
 		input.dispatchEvent(new Event('change', { bubbles: true }));
 	}
-	
+
 	change(e) {
 		let selectedValue = e.target.value;
 		this.reset();
-		
+
 		switch (selectedValue) {
 			case 'clear':
 				this.reset();
@@ -83,51 +83,54 @@ class modMacroHandler {
 			case 'denyfiles':
 				this.set('deny', true);
 				break;
+			case 'ban':
+				this.set('global_ban', true);
+				this.set('ban_h', true);
+				this.set('preserve_post', true);
+				break;
 			case 'rule1':
 				this.set('delete_file', true);
 				this.set('global_ban', true);
-				this.set('wide_range', true);
-				this.set('preserve_post', true);
+				this.set('ban_h', true);
 				this.set('ban_reason', 'rule 1');
 				this.set('ban_duration', '10y');
-				
+				this.set('preserve_post', true);
 				this.set('untrust', true);
 				break;
 			case 'rule2':
 				this.set('delete_file', true);
 				this.set('global_ban', true);
-				this.set('wide_range', true);
-				this.set('preserve_post', true);
+				this.set('ban_h', true);
 				this.set('ban_reason', 'rule 2');
 				this.set('ban_duration', '1h');
-				
+				this.set('preserve_post', true);
 				this.set('untrust', true);
 				break;
 			case 'rule3':
 				this.set('global_ban', true);
-				this.set('wide_range', true);
-				this.set('preserve_post', true);
+				this.set('ban_h', true);
 				this.set('ban_reason', 'rule 3');
 				this.set('ban_duration', '1d');
+				this.set('preserve_post', true);
 				break;
 			case 'rule4':
 				this.set('global_ban', true);
-				this.set('wide_range', true);
-				this.set('preserve_post', true);
+				this.set('ban_h', true);
 				this.set('ban_reason', 'rule 4');
 				this.set('ban_duration', '4h');
+				this.set('preserve_post', true);
 				break;
 			case 'rule5':
 				this.set('global_ban', true);
-				this.set('wide_range', true);
-				this.set('preserve_post', true);
+				this.set('ban_h', true);
 				this.set('ban_reason', 'rule 5');
 				this.set('ban_duration', '1y');
+				this.set('preserve_post', true);
 				break;
 			case 'banevasion':
+				this.set('delete_ip_global', true);
 				this.set('global_ban', true);
-				this.set('wide_range', true);
-				this.set('preserve_post', true);
+				this.set('ban_h', true);
 				this.set('ban_reason', 'ban evasion');
 				this.set('ban_duration', '1h');
 				break;
@@ -137,7 +140,6 @@ class modMacroHandler {
 
 window.addEventListener('DOMContentLoaded', () => {
 	let dropdown = document.getElementById('modmacros');
-	
 	if (dropdown) {
 		new modMacroHandler(dropdown);
 	}
