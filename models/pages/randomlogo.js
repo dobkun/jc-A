03505d748223bc0b30465da5a9d670c2c5158416
blackchandev <183952445+anthro-main@users.noreply.github.com>
2025-05-16 17:54:23 -0700

    mock random logo

---
+++ models/pages/randomlogo.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const Assets = require(__dirname + '/../../db/assets.js');
+
+module.exports = async (req, res, next) => {
+	let logo;
+	try {
+		// banner = await Assets.randomBanner();
+	} catch (err) {
+		return next(err);
+	}
+
+	if (!logo) {
+		//non existing boards will show default banner, but it doesnt really matter.
+		return res.redirect('/file/desulogo.png');
+	}
+
+	return res.redirect('/file/desulogo.png');
+
+};
