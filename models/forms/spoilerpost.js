8446f0ba1ee73d5dacb52b9583df28d767e83a7a
blackchandev <183952445+anthro-main@users.noreply.github.com>
2025-05-18 22:17:26 -0700

    log individual spoiler actions

--- models/forms/spoilerpost.js
+++ models/forms/spoilerpost.js
@@ -6,23 +6,25 @@ module.exports = async (req, res) => {
 
 	const { __, __n, posts } = res.locals;
 
+	// filter to ones not spoilered
+	const filteredPosts = posts.filter(post => {
+		return !post.spoiler && post.files.length > 0;
+	});
+
+	if (filteredPosts.length === 0) {
+		return {
+			message: __('No files to spoiler'),
+		};
+	}
+
 	if (req.body.file_action_filename) {
 		await Posts.spoilerFile(req.body.file_action_filename);
 		return {
-			message: __n('Spoilered 1 file')
+			message: __n('Spoilered 1 file'),
+			action: '$set',
+			query: {}
 		};
 	} else {
-		// filter to ones not spoilered
-		const filteredPosts = posts.filter(post => {
-			return !post.spoiler && post.files.length > 0;
-		});
-
-		if (filteredPosts.length === 0) {
-			return {
-				message: __('No files to spoiler'),
-			};
-		}
-
 		return {
 			message: __n('Spoilered %s posts', filteredPosts.length),
 			action: '$set',
